6f4ae409ef252b12a5fc4102b00799598a7aef9e
aziz <azomndr@gmail.com>
2022-01-23 20:10:14 +0800

    final commit!

--- server/index.js
+++ server/index.js
@@ -14,8 +14,18 @@ const io = new Server(server, {
   },
 });
 
+//SECTION: SOCKET IO
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
